ee6fa3473ecfb1cfe8ccfd5d9a6a36ce65415711
dring1 <devon.ring@gmail.com>
2013-11-13 13:03:28 -0500

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FAuth::Application.config.secret_key_base = '89ef228d8d99d0f49c60aca9485e9bf45935ce77ce69f8dfeeca18eb841beff488e59728085223c25db3a957188db6a74aabbe8b5f12eb147c129720c9cb640c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FAuth::Application.config.session_store :cookie_store, key: '_fAuth_session'
