a9da25186e8c89b73abfb94c6ee61b7e0f93ce6b
dring1 <devon.ring@gmail.com>
2013-11-23 22:52:18 -0500

    oops, forgot add

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+	def new
+		
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		file = params[:session][:checksum]
+		if file
+			cs   = Digest::SHA512.hexdigest( File.read(file.path))
+		end
+		if user && user.authenticate(params[:session][:password]) && user.valid_checksum(cs)
+			flash.now[:success] = "Gratz bro"
+			#signin user
+			redirect_to user
+		# Sign the user in and redirect to the user's show page.
+		else
+			flash.now[:error] = 'Invalid email/password/file combination' # Not quite right!
+      		render 'new'
+		end
+	end
+
+	def destroy
+		
+	end
+
+
+
+end
